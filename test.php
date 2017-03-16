f062bb47e01560981184cb47b2523fc817d86a82
Jean Paul Ruiz <jpruiz114@gmail.com>
2017-03-16 15:36:43 -0400

    Test

--- test.php
+++ test.php
@@ -7,6 +7,15 @@ use TheDMSGrp\ZipGeo\GeoNamesOrgParser;
 try {
     $result = GeoNamesOrgParser::getGeoInfoByCityAndStateName('Clearwater', 'FL');
     print_r($result);
+
+    $result = GeoNamesOrgParser::getGeoInfoByCityAndStateName('Clearwater', null);
+    print_r($result);
+
+    $result = GeoNamesOrgParser::getResultFromDB(['city' => 'CLEARWATER', 'state-code' => 'FL']);
+    print_r($result);
+
+    $result = GeoNamesOrgParser::getResultFromDB(['zipcode' => '33761']);
+    print_r($result);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
